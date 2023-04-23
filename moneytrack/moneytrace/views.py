135f4d587632371c7a03ccad391f88b9d4d4bc2d
Muhammad Raihan <mraihanfs19@gmail.com>
2023-04-24 02:53:58 +0700

    add new checking session on login view 24042023

--- moneytrack/moneytrace/views.py
+++ moneytrack/moneytrace/views.py
@@ -5,7 +5,8 @@ from django.views import View, generic
 from .validation import validate_transaction_type, validate_is_number
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
-import json
+from django.contrib.sessions.models import Session
+from django.utils import timezone
 # Create your views here.
 
 
@@ -17,7 +18,7 @@ def debet_or_credit(transactionType, value):
         return [0, value]
     elif transactionType == 'C':
         return [value, 0]
-    
+
 
 def sum_of_transaction(valueCredit, valueDebet):
     sumOfTransaction = valueCredit - valueDebet
@@ -32,6 +33,15 @@ def get_every_field(model):
     return [field.name for field in model._meta.fields]
 
 
+def check_if_account_had_session(user):
+    unexpired_sessions = Session.objects.filter(
+        expire_date__gte=timezone.now())
+    for session in unexpired_sessions:
+        if str(user.pk) == session.get_decoded().get('_auth_user_id'):
+            return True
+    return False
+
+
 @require_http_methods(['GET'])
 class IndexView (generic.ListView):
     context_object_name = 'latest_transasction'
@@ -40,28 +50,41 @@ class IndexView (generic.ListView):
     def get_queryset(self):
         return Transaction.objects.order_by('transaction_date')[-5]
 
+
 class LoginView(View):
     def post(self, req):
         username = req.POST.get('username')
         password = req.POST.get('password')
         user = authenticate(req, username=username, password=password)
-        if user.is_authenticated:
-            return HttpResponse("User sedang login mohon menggunakan akun yang lain", status=403)
-        else:
-            if user is not None:
-                req.session.set_expiry(1800)
-                login(req, user=user)
+        if user is not None:
+            if check_if_account_had_session(user):
                 response = {
-                    "status": "202",
-                    "message": "Berhasil Login"
+                    "message": "User sedang login",
+                    "status": 403,
                 }
-                return JsonResponse(response)
-            else:
-                return HttpResponse("Username atau password salah", status=403)
+                return JsonResponse(response, status=403)
+            req.session.set_expiry(1800)
+            login(req, user=user)
+            response = {
+                "message": "Berhasil Login",
+                "status": 403,
+            }
+            return JsonResponse(response, status=202)
+        else:
+            response = {
+                "message": "Username atau password tidak ditemukan",
+                "status": 403,
+            }
+            return JsonResponse(response, status=403)
+
 
 class LogoutView(View):
     def get(self, req):
+        for session in Session.objects.filter(expire_date__gte=timezone.now()):
+            # Clear the contents of each session
+            session.delete()
         return HttpResponse(logout(req))
+
 
 class TransactionView(LoginRequiredMixin, View):
     raise_exception = True
@@ -75,17 +98,17 @@ class TransactionView(LoginRequiredMixin, View):
                             'transactionType': d[3], 'valueDebet': d[4], 'valueCredit': d[5], 'sum_value': d[6]})
         print(request.user)
         return JsonResponse(dataJson, safe=False)
-    
-    def post (self, req):
+
+    def post(self, req):
         data = req.POST
         try:
             description = str(data['description'])
             transactionType = str(data['transactionType'])
             value = data['value']
             value = debet_or_credit(transactionType, value)
-            sum_value = sum_of_transaction(value[0], value[1])    
+            sum_value = sum_of_transaction(value[0], value[1])
             newTransaction = Transaction(description=description, transactionType=transactionType,
-                                        valueCredit=value[0], valueDebet=value[1], sum_value=sum_value)
+                                         valueCredit=value[0], valueDebet=value[1], sum_value=sum_value)
             newTransaction.save()
             return HttpResponse(newTransaction.__str__)
         except Exception as e:
